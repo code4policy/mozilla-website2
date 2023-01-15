0859b3cd6ab89ff7da86674fc73171626a08562f
navneet <navneetk12@gmail.com>
2023-01-15 17:26:31 -0500

    add new features

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,20 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
+ // Define a Function
+ function sayOuch() {
+ 	alert('Ouch! Stop poking me!');
+ }
+ 
+ // Use a CSS selector to identify an element
+ var foxImage = document.querySelector('img');
+ 
+ // Assign the function to the onclick event on that element
+ foxImage.onclick = sayOuch;
+ function toggleImage() {
+  var image = document.getElementById("my-image");
+  if (image.style.display === "none") {
+    image.style.display = "block";
+  } else {
+    image.style.display = "none";
+  }
+}
